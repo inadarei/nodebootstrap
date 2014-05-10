296c97d2f3a069ec8c3172b22ffefa4f452918b8
Irakli N <irakli@gmail.com>
2014-05-10 04:49:59 -0400

    cleaning up server.js in prep for major modularization

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('./lib/app').setup(function(runningApp) {
 
-  //---- Mounting application modules
+  //---- Mounting well-encapsulated application modules
+  //---- See: http://vimeo.com/56166857
+
   runningApp.use('/hello', require('./lib/hello')); // attach to sub-route
   runningApp.use(require('./lib/routes')); // attach to root route
 
