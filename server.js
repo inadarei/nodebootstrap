779a565958aa5ce6ccdfac63b45088ad67220dc3
Irakli N <irakli@gmail.com>
2015-02-01 16:21:13 -0500

    initial fix

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-require('nodebootstrap-server').setup(function(runningApp) {
+var server = require('nodebootstrap-server');
 
+server.setup(function(runningApp) {
+  
+  // runningApp.use(require('express-session')({secret: CONF.app.cookie_secret, resave: false, saveUninitialized: false}));
+  
   //---- Mounting well-encapsulated application modules
   //---- See: http://vimeo.com/56166857
 
   runningApp.use('/hello', require('hello')); // attach to sub-route
   runningApp.use(require('routes')); // attach to root route
-
+  
 });
