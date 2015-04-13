7b911380dc56e7b728d5baa149900c0855401546
Irakli Nadareishvili <inadarei@users.noreply.github.com>
2015-04-13 13:52:26 -0400

    removing hbs as a hard dependency from nb-server and moving into project space

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ server.setup(function(runningApp) {
   
   // runningApp.use(require('express-session')({secret: CONF.app.cookie_secret, resave: false, saveUninitialized: false}));
   
+  // Choose your favorite view engine(s)  
+  runningApp.set('view engine', 'handlebars');
+  runningApp.engine('handlebars', require('hbs').__express);
+
+  //// you could use two view engines in parallel (if you are brave):  
+  // runningApp.set('view engine', 'j2');
+  // runningApp.engine('j2', require('swig').renderFile);
+
+
   //---- Mounting well-encapsulated application modules
   //---- See: http://vimeo.com/56166857
 
