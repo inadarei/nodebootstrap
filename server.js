ef1d30f6042f062eb0778e9ff7c73cadcd4b87d5
Irakli Nadareishvili <inadarei@users.noreply.github.com>
2015-10-23 02:31:52 -0700

    adding lib to NODE_PATH for nicer code, with add-module-path without having to use NODE_PATH env vars. env vars are so 2012

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// @see: https://gist.github.com/branneman/8048520
+require('app-module-path').addPath(__dirname + '/lib');
+
 var server = require('nodebootstrap-server');
 
 server.setup(function(runningApp) {
