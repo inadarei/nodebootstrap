c2b047077770680b80082bbb96d8b4a3130223bf
jnelson <jnelson11@gmail.com>
2016-02-26 16:33:13 -0500

    Fixing an invalid file reference

--- test/support/server.js
+++ test/support/server.js
@@ -1,6 +1,6 @@
 var server = require('nodebootstrap-server')
   , express = require('express')
-  , appConfig = require('../../app');
+  , appConfig = require('../../appConfig');
 
 exports.beforeEach = function(app, callback) {
   server.setupTest(app, function(app) {
