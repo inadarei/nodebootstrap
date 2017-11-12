91c6af370444f1a56f9742dcded1e774cf5cfeeb
Irakli N <inadarei@users.noreply.github.com>
2017-11-12 18:46:53 -0500

    adding blank test

---
+++ test/blank.js
@@ -0,0 +1,10 @@
+const assert  = require('chai').assert;
+
+describe('Blank example', function() {
+
+    it('Blank test', function(done) {
+        assert.equal(true,true);
+        done();
+    });
+
+});
